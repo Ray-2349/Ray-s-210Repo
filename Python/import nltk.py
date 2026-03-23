b31a4491ee6c8069ba53c97dcc92c8160a8a822b
Raymond Brown  III <rkb5707@psu.edu>
2026-03-23 13:20:03 -0400

    notebook

---
+++ Python/import nltk.py
@@ -0,0 +1,11 @@
+import nltk
+import nltk.corpus
+# The next line downloads all the example texts used in the NLTK book at https://www.nltk.org/book !
+# You can comment out the download line after the first time you do it.
+nltk.download('book')
+from nltk.book import *
+# The next line lets us do GET requests from remote URLs on the web:
+from urllib import request
+# The following import lines are for plotting interactive visualizations in Python
+import matplotlib
+import matplotlib.pyplot as plt
